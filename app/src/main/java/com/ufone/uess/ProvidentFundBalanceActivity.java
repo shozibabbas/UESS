13ae39357befe0ae23f200f6300c62d4efc7ab65
shozibabbas <14bscssabbas@seecs.edu.pk>
2017-07-25 10:36:18 +0500

    adding provident fund balance

---
+++ app/src/main/java/com/ufone/uess/ProvidentFundBalanceActivity.java
@@ -0,0 +1,13 @@
+package com.ufone.uess;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProvidentFundBalanceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provident_fund_balance);
+    }
+}
